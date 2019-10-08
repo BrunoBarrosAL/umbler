e3f1793306f74cb926659b5363393f628849d0f7
bruno <brunobarros.al@hotmail.com>
2019-10-07 22:32:30 -0300

    Arquivo modificado

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Exemplo de deply automático na Umbler</title>
+</head>
+<body>
+<h1>Deu certo!</h1>
 <?php 
   phpinfo();
 ?>
+</body>
+</html>
